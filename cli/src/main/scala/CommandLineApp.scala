2aebaf9b829505f55e58855285b6a75b853b06c6
Anton Sviridov <keynmol@gmail.com>
2022-11-21 12:45:59 +0000

    Fix formatting

--- cli/src/main/scala/CommandLineApp.scala
+++ cli/src/main/scala/CommandLineApp.scala
@@ -1,8 +1,8 @@
 package com.indoorvivants.vcpkg
 package cli
 
-import cats.implicits._
-import com.monovore.decline._
+import cats.implicits.*
+import com.monovore.decline.*
 import java.io.File
 
 enum Action:
@@ -54,14 +54,40 @@ object Options extends VcpkgPluginImpl:
         .map[VcpkgRootInit](allow => VcpkgRootInit.SystemCache(allow))
     )
 
-  private val vcpkgInstallDir = Opts.option[String]("vcpkg-install", metavar = "dir", help = "folder where packages will be installed")
-  .map(new File(_)).withDefault(defaultInstallDir)
+  private val vcpkgInstallDir = Opts
+    .option[String](
+      "vcpkg-install",
+      metavar = "dir",
+      help = "folder where packages will be installed"
+    )
+    .map(new File(_))
+    .withDefault(defaultInstallDir)
 
-  private val verbose = Opts.flag(long = "verbose", short = "v", visibility = Visibility.Normal, help = "Verbose logging").orFalse
+  private val verbose = Opts
+    .flag(
+      long = "verbose",
+      short = "v",
+      visibility = Visibility.Normal,
+      help = "Verbose logging"
+    )
+    .orFalse
 
-  private val action = Opts.options[String]("install", help = "list of dependencies to install").map(_.toList).map(Action.Install(_)).orElse(
-    Opts.option[String]("install-manifest", help = "install dependencies specified by the manifest file (e.g. vcpkg.json)", metavar = "file").map(new File(_)).validate("File should exist")(f => f.exists() && f.isFile()).map(Action.InstallManifest(_))
-  )
+  private val action = Opts
+    .options[String]("install", help = "list of dependencies to install")
+    .map(_.toList)
+    .map(Action.Install(_))
+    .orElse(
+      Opts
+        .option[String](
+          "install-manifest",
+          help =
+            "install dependencies specified by the manifest file (e.g. vcpkg.json)",
+          metavar = "file"
+        )
+        .map(new File(_))
+        .validate("File should exist")(f => f.exists() && f.isFile())
+        .map(Action.InstallManifest(_))
+    )
 
   val logger = ExternalLogger(
     debug = scribe.debug(_),
@@ -71,46 +97,56 @@ object Options extends VcpkgPluginImpl:
   )
 
   case class Config(
-    rootInit: VcpkgRootInit,
-    installDir: File,
-    action: Action,
-    allowBootstrap: Boolean,
-    verbose: Boolean
+      rootInit: VcpkgRootInit,
+      installDir: File,
+      action: Action,
+      allowBootstrap: Boolean,
+      verbose: Boolean
   )
 
-  val opts = Command(name, header)((vcpkgRootInit, vcpkgInstallDir, action, vcpkgAllowBootstrap, verbose).mapN(Config.apply))
-
-
+  val opts = Command(name, header)(
+    (vcpkgRootInit, vcpkgInstallDir, action, vcpkgAllowBootstrap, verbose).mapN(
+      Config.apply
+    )
+  )
+end Options
 
 object VcpkgCLI extends VcpkgPluginImpl:
-  import Options._
-  
-  def main(args: Array[String]): Unit = 
+  import Options.*
+
+  def main(args: Array[String]): Unit =
     opts.parse(args) match
       case Left(help) =>
         System.err.println(help)
-        if help.errors.nonEmpty then
-          sys.exit(1)
+        if help.errors.nonEmpty then sys.exit(1)
         else sys.exit(0)
       case Right(config) =>
         import config.*
-        if verbose then 
+        if verbose then
           scribe.Logger.root.withMinimumLevel(scribe.Level.Trace).replace()
+
         val root = rootInit.locate(logger).fold(sys.error(_), identity)
         scribe.debug(s"Locating/bootstrapping vcpkg in ${root.file}")
+
         val binary = vcpkgBinaryImpl(root, logger)
         scribe.debug(s"Binary is $binary")
+
         val manager = VcpkgBootstrap.manager(binary, installDir, logger)
 
         action match
-          case Action.Install(dependencies) => 
-            scribe.info("Installed dependencies: ", 
-              vcpkgInstallImpl(dependencies.toSet, manager, logger).map(_._1.name).mkString(", ")
-              )
-          case Action.InstallManifest(file) =>
-            scribe.info("Installed dependencies: ", 
-              vcpkgInstallManifestImpl(file, manager, logger).map(_._1.name).mkString(", ")
+          case Action.Install(dependencies) =>
+            scribe.info(
+              "Installed dependencies: ",
+              vcpkgInstallImpl(dependencies.toSet, manager, logger)
+                .map(_._1.name)
+                .mkString(", ")
             )
-        
-
-
+          case Action.InstallManifest(file) =>
+            scribe.info(
+              "Installed dependencies: ",
+              vcpkgInstallManifestImpl(file, manager, logger)
+                .map(_._1.name)
+                .mkString(", ")
+            )
+        end match
+end VcpkgCLI
