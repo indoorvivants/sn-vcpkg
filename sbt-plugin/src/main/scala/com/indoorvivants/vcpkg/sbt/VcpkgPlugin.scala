dea0e0b15d9985061075008df927de57c8f3288d
Anton Sviridov <keynmol@gmail.com>
2022-12-07 12:54:50 +0000

    SBT task caching

--- sbt-plugin/src/main/scala/com/indoorvivants/vcpkg/sbt/VcpkgPlugin.scala
+++ sbt-plugin/src/main/scala/com/indoorvivants/vcpkg/sbt/VcpkgPlugin.scala
@@ -12,6 +12,7 @@ import java.nio.file.Files
 import java.util.Arrays
 import java.util.stream.Collectors
 import scala.sys.process
+import sjsonnew.JsonFormat
 
 object VcpkgPlugin extends AutoPlugin with vcpkg.VcpkgPluginImpl {
 
@@ -45,6 +46,20 @@ object VcpkgPlugin extends AutoPlugin with vcpkg.VcpkgPluginImpl {
   }
 
   import autoImport._
+
+  import sbt.util.CacheImplicits._
+
+  implicit val depFmt: JsonFormat[vcpkg.Dependency] =
+    caseClass2(vcpkg.Dependency.apply, vcpkg.Dependency.unapply)(
+      "name",
+      "features"
+    )
+
+  implicit val fiFmt: JsonFormat[vcpkg.FilesInfo] =
+    caseClass2(vcpkg.FilesInfo.apply, vcpkg.FilesInfo.unapply)(
+      "includeDir",
+      "libDir"
+    )
 
   override lazy val projectSettings = Seq(
     vcpkgDependencies := Set.empty,
@@ -87,16 +102,44 @@ object VcpkgPlugin extends AutoPlugin with vcpkg.VcpkgPluginImpl {
     vcpkgInstall := {
       val manager = vcpkgManager.value
       val logger = sbtLogger(sLog.value)
+      val cacheFactory = streams.value.cacheStoreFactory
+      val dependencies = vcpkgDependencies.value.toList.sorted
 
-      vcpkgManifest.?.value.foreach { file =>
-        vcpkgInstallManifestImpl(file, manager, logger)
-      }
-
-      vcpkgInstallImpl(
-        dependencies = vcpkgDependencies.value,
-        manager,
-        logger
+      val cachedInstall = Cache.cached(
+        cacheFactory.make("sbt-vcpkg-vcpkg-dependencies-install")
+      )((deps: List[String]) =>
+        vcpkgInstallImpl(dependencies = deps.toSet, manager, logger).toList
       )
+
+      val manifestFile = vcpkgManifest.?.value
+
+      type ManifestIn = Option[(File, HashFileInfo)]
+      type StuffOut = List[(vcpkg.Dependency, vcpkg.FilesInfo)]
+
+      val cachedManifestInstall =
+        Tracked.lastOutput(cacheFactory.make("sbt-vcpkg-manifest-last")) {
+          (min: ManifestIn, lastOutput: Option[StuffOut]) =>
+            val f: ManifestIn => StuffOut =
+              Tracked.inputChanged[ManifestIn, StuffOut](
+                cacheFactory.make("sbt-vcpkg-input-changed")
+              ) { (changed: Boolean, in: ManifestIn) =>
+                if (changed) {
+                  in.map(_._1)
+                    .map { file =>
+                      vcpkgInstallManifestImpl(file, manager, logger).toList
+                    }
+                    .getOrElse(Nil)
+                } else lastOutput.getOrElse(List.empty)
+              }
+
+            f(min)
+        }
+
+      cachedManifestInstall(
+        vcpkgManifest.?.value.map(f => f -> FileInfo.hash(f))
+      )
+
+      cachedInstall(dependencies).toMap
     }
   )
 
